7641e32f65f5b8e6dbe2fbf8c263001262026091
timcrinion <74121932+timcrinion@users.noreply.github.com>
2022-04-23 00:47:05 +0100

    Update poly.h
    capital P

--- poly.h
+++ poly.h
@@ -1,6 +1,6 @@
 
 
-static class poly{
+static class Poly{
 	public:
 		float QuadraticFormula(float a, float b, float c); //find x such that ax²+bx+c=0
 		float CubicFormula(float a, float b, float c, float d); //find x such that a³x+bx²+cx+d=0
