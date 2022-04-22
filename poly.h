0aef5d7c89276d4ac400136b085bda02ea3dabd3
timcrinion <74121932+timcrinion@users.noreply.github.com>
2022-04-23 00:39:20 +0100

    Create poly.h
    declare first two formulas

---
+++ poly.h
@@ -0,0 +1,7 @@
+
+
+static class poly{
+	public:
+		float QuadraticFormula(float a, float b, float c); //find x such that ax²+bx+c=0
+		float CubicFormula(float a, float b, float c, float d); //find x such that a³x+bx²+cx+d=0
+};
