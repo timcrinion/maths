60722f9d4b80ef861c27152e471f13257f5010ac
timcrinion <74121932+timcrinion@users.noreply.github.com>
2022-04-23 23:52:39 +0100

    Create Poly.cpp
    Added Poly cpp file

---
+++ Poly.cpp
@@ -0,0 +1,13 @@
+#include "Poly.h"
+
+float Poly::QuadraticFormula(float a, float b, float c)
+{
+	//ax^2 + bx + c = 0	
+	
+}
+
+//solves x for x^2 + bx + c = 0
+float QuadraticFormula_specific(float b, float c)
+{
+
+}
