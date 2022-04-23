4b83745fb79deb67b2c14a994670ba4809f040a3
timcrinion <74121932+timcrinion@users.noreply.github.com>
2022-04-24 00:09:17 +0100

    Update Poly.cpp

--- Poly.cpp
+++ Poly.cpp
@@ -3,7 +3,11 @@
 float Poly::QuadraticFormula(float a, float b, float c)
 {
 	//ax^2 + bx + c = 0	
+<<<<<<< Updated upstream
 	//something here 2
+=======
+	//something here 1
+>>>>>>> Stashed changes
 }
 
 //solves x for x^2 + bx + c = 0
