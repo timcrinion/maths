f0b1681f6900b1014bef56b660dcc689e163f412
timcrinion <74121932+timcrinion@users.noreply.github.com>
2024-11-09 20:06:12 +0000

    Update Linear.h

--- Linear.h
+++ Linear.h
@@ -3,5 +3,5 @@ static class Linear
 public:
   // calculate x such that mx+c=0
   // return true if suceeded, false if failed
-  bool solveLinear(double m, double c, double& outX);
+  bool SolveLinear(double m, double c, double& outX);
 }
