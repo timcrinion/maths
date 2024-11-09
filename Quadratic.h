ed974ed7ffb1b8389b9463506c0b9d201b828774
timcrinion <74121932+timcrinion@users.noreply.github.com>
2024-11-09 20:09:17 +0000

    Create Quadratic.h

---
+++ Quadratic.h
@@ -0,0 +1,7 @@
+static class Quadratic
+{
+public:
+  // calculate x such that a^2 x + bx + c = 0
+  // return true if suceeded, false if failed
+  bool SolveQuadratic(double a, double b, double c, double& outX);
+}
